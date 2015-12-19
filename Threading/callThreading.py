59102990f3c613298359e4624c1bcbd3687443b7
mma <majingrzl@163.com>
2015-12-19 11:21:24 +0800

    threading test

---
+++ Threading/callThreading.py
@@ -0,0 +1,35 @@
+#-*- coding:utf-8 -*-
+"""创建一个Thread类实例，传入一个可调用的类对象"""
+import time, threading  
+loops=[4,2]  
+  
+class MyThread(object):  
+      def __init__(self, func, args,name=""):  
+          self.args = args  
+          self.func = func  
+          self.name = name  
+  
+      def __call__(self, *args, **kwargs):  
+          self.func(*self.args)  
+  
+def loop(nloop, nsec):  
+    print "loop starts:%d"%(nloop) + "  %s\n"%(time.ctime())  
+    time.sleep(nsec)  
+    print "loop ends:%d"%(nloop) + "  %s\n"%(time.ctime())  
+  
+def main():  
+    threads =[]  
+    for i in range(len(loops)):  
+        tt = threading.Thread(target=MyThread(loop, (i,loops[i]),loop.__name__))  #仅修改了这里  
+        print "线程名字：",tt.getName()  
+        threads.append(tt)  
+    for i in range(len(loops)):  
+        threads[i].start()  
+  
+    for item in threads:  
+        item.join()  
+  
+    print "all done:%s\n"%(time.ctime())  
+  
+if __name__ == "__main__":  
+    main()  
---
+++ Threading/insThreading.py
@@ -0,0 +1,42 @@
+# -*- coding:utf-8  -*-
+"""创建一个threading.Thread的实例，传进一个函数"""
+import threading, time
+
+loops=[4,2]  
+
+def func(index, sec):  
+
+      print "func starts:%d"%(index)+"  %s\n"%(time.ctime())  
+      
+      time.sleep(sec) 
+      
+      print "func ends:%d"%(index)+"  %s\n"%(time.ctime())  
+      
+      
+def main():  
+
+     threads =[]  
+     
+     nloop = range(nlen(loops))  
+     
+     for i in nloop:  
+     
+          tt = threading.Thread(target=func, args=(i, loops[i]))  
+          
+          print "the name of the threadind:%s\n"%(tt.getName())  
+          
+          threads.append(tt)  
+    
+     for i in nloop:  
+     
+          threads[i].start()  
+  
+     for i in nloop: 
+     
+           threads[i].join()  #允许主线程等待所有的线程运行完之后在执行后面的逻辑  
+           
+     print "all done:%s\n"%(time.ctime())  
+  
+if __name__ == "__main__":  
+
+     main()  
