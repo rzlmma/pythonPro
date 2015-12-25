54351e25e25d6263547a675f872637d68edc3b75
mma <majingrzl@163.com>
2015-12-25 18:38:35 +0800

    add a function of generator

--- pythonTest/code1.py
+++ pythonTest/code1.py
@@ -1,37 +1,40 @@
 # -*- coding:utf-8 -*-
-"""  打印出1-1亿之间的偶数
+"""  打印出1-100之间的偶数
 """
 from __future__ import print_function
 import time
 
 # for loop
 def forLoop():
-    start = time.clock()
-    for i in xrange(1,100000001):
+    for i in xrange(1,101):
         if i%2 == 0:
             print(i,end=',')
-    res = time.clock()-start
-    return res
 
 #列表解析
 def func():
-    start = time.clock()
-    item = [i for i in xrange(1,100000001) if i%2 == 0]
+    item = [i for i in xrange(1,101) if i%2 == 0]
     print (item)
-    res = time.clock() - start
-    return res
+
 
 #map
 def mapFunc():
-    start = time.clock()
-    item = filter(lambda i: i%2== 0,range(1,100000001))
+    item = filter(lambda i: i%2== 0,range(1,101))
     print(item)
-    res = time.clock()-start
-    return res
+
+#生成器
+def generate():
+    for i in (item for item in range(101) if item%2 == 0):
+        print (i,end=',')
+
+#计算耗时函数
+def timeCost(func):
+    print ("<function name:%s>"%(func.__name__))
+    start = time.clock()
+    func()
+    timecost = time.clock() - start
+    print(timecost)
 
 if __name__ == "__main__":
-    forLoop()
-    print("----"*6)
-    func()
-    print("----"*6)
-    mapFunc()
+    print("timecost:------------------------------")
+    for item in [forLoop,func,mapFunc,generate]:
+        timeCost(item)
