d253261af6dde11b05fd54dba9d4319e3d42456a
mma <majingrzl@163.com>
2016-01-02 16:39:21 +0800

    add a new function

--- file/classityFile.py
+++ file/classityFile.py
@@ -13,9 +13,11 @@ class classifyFile(object):
         print "比较时间:",self.compareTime
         fileInfo ={}
         rest = []
+        if not os.path.exists(self.dirpath):
+            raise ValueError('please check the path! path is not exists!!!')
         for dirname,dirs,files in os.walk(self.dirpath):
             for item in files:
-                file_ctime = datetime.datetime.fromtimestamp(os.path.getctime(item))
+                file_ctime = datetime.datetime.fromtimestamp(os.path.getctime(os.path.join(dirname,item)))
                 print "文件名:%s   文件创建时间:%s"%(item,file_ctime)
                 if file_ctime > self.compareTime:
                     rest.append(item)
@@ -26,23 +28,47 @@ class classifyFile(object):
     def filterFiles(self,*pattern):
         fileSet = {}
         rest = []
+        if not os.path.exists(self.dirpath):
+            raise ValueError('please check the path! path is not exists!!!')
         for dirname, dirs,files in os.walk(self.dirpath):
             for file in files:
                 items = file.split('.')
+                if len(items)<2:
+                    continue
                 if items[1] in pattern:
                     rest.append(file)
             fileSet[dirname] = rest
         return fileSet
 
+    #获取文件的信息：路径，大小，修改时间,返回排序后的文件
+    def getFileInfo(self, param):
+        fileInfo ={}
+        if not os.path.exists(self.dirpath):
+            raise ValueError('please check the path! path is not exists!!!')
+        for root, dirs, files in os.walk(self.dirpath):
+            for item in files:
+                file_name = os.path.join(root,item)
+                file_size = os.path.getsize(os.path.join(root,item))
+                file_mtime = datetime.datetime.fromtimestamp(os.path.getmtime(os.path.join(root,item))).strftime('%Y-%m-%d  %H:%M:%S')
+                fileInfo[file_name] = {'file_size': file_size, 'file_mtime': file_mtime}
 
+        fileInfo = sorted(fileInfo.items(), key=lambda x: x[1].get(param, ' '), reverse=False)
+        return fileInfo
 
 if __name__ == "__main__":
     dirs = os.getcwd()
     print "当前工作目录:",dirs
     compareTime = datetime.datetime(2015,1,1,10,0)
-    file_classify = classifyFile(compareTime,dirs)
+    file_classify = classifyFile(compareTime, r'f:\pythonPro\pythonPro')
+
+    print "------------在给定时间之后创建的文件--------"
     files = file_classify.fileLaterThan()
     for item in files.items():
         print "dirname:%s  filename:%s"%(item[0],item[1])
 
+    print "---------过滤出py,html文件-------------"
     print file_classify.filterFiles('py','html')
+
+    print "-------排列-------------"
+    for item in file_classify.getFileInfo('file_size'):
+        print item
