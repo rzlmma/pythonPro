d016c72a5399d667365ffa72b8f0accf3fcf91db
mma <majingrzl@163.com>
2016-01-09 12:42:20 +0800

    test

---
+++ test/test.py
@@ -0,0 +1,36 @@
+# -*- coding:utf-8 -*-
+"""
+a.txt内容如下。xx为汉字。若最后一个数字相同，则按每一行第一个数字为准从小到大排序
+2877,xx,xx,xx,xx,xx,xx,10
+2878,xx,xx,xx,xx,xx,1264
+2880,xx,xx,xx,xx,xx,xx,6
+2881,xx,xx,xx,xx,xx,xx,xx,248
+2882,xx,1
+2885,xx,xx,xx,xx,1
+2886,xx,xx,xx,xx,5
+2887,xx,xx,xx,1
+希望得到的结果存到b.txt 里边。内容如下：
+2878,xx,xx,xx,xx,xx,1264
+2881,xx,xx,xx,xx,xx,xx,xx,248
+2877,xx,xx,xx,xx,xx,xx,10
+2880,xx,xx,xx,xx,xx,xx,6
+2886,xx,xx,xx,xx,5
+2882,xx,1
+2885,xx,xx,xx,xx,1
+2887,xx,xx,xx,1
+"""
+
+rest = []
+with open('spam.txt') as f:
+    for line in f:
+       linelist = line.strip('\n').split(',')
+       line_rest = []
+       for item in linelist:
+           line_rest.append(int(item))
+       rest.append(line_rest)
+re_rest = sorted(rest, key=lambda x: x[-1],reverse=True)
+
+with open('b.txt','w') as fb:
+    for item in re_rest:
+        item = str(item).strip('[ ]')
+        fb.write(item+'\n')
