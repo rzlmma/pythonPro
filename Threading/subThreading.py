6c4ff070d16e881f44c7c2f7352f53c815737b88
mma <1079205598@qq.com>
2017-02-21 16:28:01 +0800

    多线程: 修改子类化线程

--- Threading/subThreading.py
+++ Threading/subThreading.py
@@ -1,34 +1,36 @@
 # -*- coding:utf-8 -*-
 """子类化Thread类"""
-import threading,time
-loops=[4,2]
+import threading, time
+loops = [4, 2]
+
 
 class MyThread(threading.Thread):
-         def __init__(self,index, sec):  
-             threading.Thread.__init__(self)  
-             self.index = index  
-             self.sec = sec  
-      
-         def run(self):  
-             print "loop starts:%d"%(self.index) + "  %s\n"%(time.ctime())  
-             time.sleep(self.sec)  
-             print "loop ends:%d"%(self.sec) + "  %s\n"%(time.ctime())  
-      
+    def __init__(self, index, sec):
+        super(MyThread, self).__init__(self)
+        self.index = index
+        self.sec = sec
+
+    def run(self):
+        print "loop starts:%d"%(self.index) + "  %s\n"%(time.ctime())
+        time.sleep(self.sec)
+        print "loop ends:%d"%(self.sec) + "  %s\n"%(time.ctime())
+
+
 def main():
-        nloop = range(len(loops))  
-        threads = []  
-        for i in nloop:  
-            tt = MyThread(i,loops[i])  
-            print "线程名字：",tt.getName()  
-            threads.append(tt)  
-        for i in range(len(loops)):  
-            threads[i].start()  
-      
-        for item in threads:  
-            item.join()  
-      
-        print "all done:%s\n"%(time.ctime())  
+    nloop = range(len(loops))
+    threads = []
+    for i in nloop:
+        tt = MyThread(i,loops[i])
+        print "线程名字：",tt.getName()
+        threads.append(tt)
+    for i in range(len(loops)):
+        threads[i].start()
+
+    for item in threads:
+        item.join()
+
+    print "all done:%s\n"%(time.ctime())
       
       
 if __name__ == "__main__":
-        main()  
+    main()
