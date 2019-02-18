e2490f429f465e1c4590f5f5ecd273a60a988893
majing <majing@gaosiedu.com>
2019-02-18 17:36:45 +0800

    字符串全排列

--- algorithm/str_all_permutation.py
+++ algorithm/str_all_permutation.py
@@ -4,21 +4,18 @@
 rest = []
 
 def CalcAllPermutation(alist, begin, to):
-    if to == 1:
-        return
-
     if begin == to:
-        rest.append(alist[0:to+1])
+        rest.append(alist[:])      # 这里不能是alist,如果是alist,所有的结果都一样
 
     else:
         for j in range(begin, to+1):
             alist[j], alist[begin] = alist[begin], alist[j]
             CalcAllPermutation(alist, begin+1, to)
-            alist[j], alist[begin] = alist[begin], alist[j]
+            alist[j], alist[begin] = alist[begin], alist[j]     # 最后在交换过来
 
 
 
 
 if __name__ == "__main__":
-    CalcAllPermutation(['a','b','c', 'd'], 0,3)
+    CalcAllPermutation(['a'], 0,0)
     print("rest: %s\n       len(rest):%s" % (rest, len(rest)))
