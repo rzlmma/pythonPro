18820d48141ab5d3a64463706e662a14b8a89386
majing <majing@gaosiedu.com>
2019-03-05 10:48:58 +0800

    just test

--- algorithm/str_all_permutation.py
+++ algorithm/str_all_permutation.py
@@ -14,8 +14,32 @@ def CalcAllPermutation(alist, begin, to):
             alist[j], alist[begin] = alist[begin], alist[j]     # 最后在交换过来
 
 
+def test(alist, begin, to, step):
+    print("begin:%s  to:%s   step:%s" % (begin, to, step))
+    if begin == to:
+        return
+
+    if step == to:
+        return
+
+    for i in range(begin, to):
+        if step == 1:
+            item = alist[i:i+step]
+        else:
+            item = alist[i:i+step-1]
+            print('item: ', item)
+            item.append(alist[begin-1])
+        rest.append(item)
+    test(alist, begin+1, to, step)
+    test(alist, begin+1, to, step+1)
+
+
+def test_1():
+    print("test_1 ....")
+
 
 
 if __name__ == "__main__":
-    CalcAllPermutation(['a'], 0,0)
+    # CalcAllPermutation(['a'], 0,0)
+    test(['a', 'b', 'c', 'd'], 0, 4, 1)
     print("rest: %s\n       len(rest):%s" % (rest, len(rest)))
